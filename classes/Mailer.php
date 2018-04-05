7f1a0d4683d6f828c12acc29643d8be6209b2de1
Salvi Pascual <salvi@techlaunch.io>
2018-04-05 18:50:20 -0400

    first commit

---
+++ classes/Mailer.php
@@ -0,0 +1,32 @@
+<?php
+
+class Mailer
+{
+	static function send($to, $subject, $body)
+	{
+		// include the config params
+		require_once "configs/configs.php";
+
+		// create message
+		$mail = new Nette\Mail\Message;
+		$mail->setFrom("noreply@$domain");
+		$mail->addTo($to);
+		$mail->setSubject($subject);
+		$mail->setHtmlBody($body, false);
+
+		// send mail
+		$mailer = new Nette\Mail\SmtpMailer([
+			'host' => $host,
+			'username' => $username,
+			'password' => $password,
+			'port' => $port,
+			'secure' => $secure
+		]);
+
+		$mailer->send($mail, false);
+
+		error_log("sent");
+
+		return true;
+	}
+}
---
+++ run.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once "vendor/autoload.php";
+include_once "classes/Mailer.php";
+
+$i = 1; // DELETE
+while(true)
+{
+	// get the next email to use
+	// @TODO
+	$to = "salvi@apretaste.com";
+
+	// get the email content
+	// @TODO
+	$id = "1234";
+	$subject = "hello world";
+	$body = "adios planeta";
+
+	// send the email
+	Mailer::send($to, $subject, $body);
+
+	// save in the log
+	// TODO
+	$dt = date("h:i:s");
+	error_log("Email $id sent to $to at $dt");
+
+	// calculate a random delay
+	sleep(rand(2, 10));
+
+	// break on time
+	if($i++ > 5) break; // DELETE
+}
+
+echo "ENDING";
